6e094e1856dfba981c05927d3e9a1e9e670b9768
Mikhail B. Nunez <mnunez@exist.com>
2019-06-20 09:53:00 +0800

    Initial commit

---
+++ src/main/java/com/demo/myspringdemoapp/MySpringDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.demo.myspringdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringDemoAppApplication.class, args);
+	}
+
+}
